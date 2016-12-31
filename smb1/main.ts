8c5e35c871846ae7606983cebdd54c9b2a85bae3
Salwan <salwanmax@gmail.com>
2016-12-31 12:46:31 +0000

    Attempt in Typescript not going well as I'm not finding the runtime system to be predictable. I have had two similar issues so far, a defined method that isn't correctly called by typescript as it seems the `this` pointer changes depending on who and where the call happens which is completely unpredictable, `this` should always remain the instance of the owning object why does that change based on who calls the method?

--- smb1/main.ts
+++ smb1/main.ts
@@ -1,37 +1,98 @@
-import "../phaser262/build/phaser";
+/// <reference path="../phaser262/typescript/phaser.d.ts" />
 
-console.log('This  is Typescript Main.');
+import '../phaser262/build/phaser.min.js';
 
-class Greeting {
-    constructor(public greeting: string) { }
-    greet() {
-        return '<h1>' + this.greeting + '</h1>';
+const WIDTH = 512.0;
+const HEIGHT = 480.0;
+const RESMULX = 2.0;
+const RESMULY = 2.0;
+
+let phaser : Phaser.Game;
+
+class SMBGame {
+    currentScene: Scene;
+
+    public constructor() {
+        phaser = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'content', { preload: this.preload, create: this.create,
+            update: this.update, render: this.render }, false, false);
+    }
+
+    public preload() {
+        phaser.stage.backgroundColor = 0x5c94fc;
+        phaser.load.image('logo', './smb1/assets/title.fw.png');
+        phaser.load.bitmapFont('emulogic', './smb1/assets/fonts/emulogic_0.png', './smb1/assets/fonts/emulogic.fnt');
+    }
+    
+    public changeScene(scene:Scene):void {
+        if(scene) {
+            if(this.currentScene) {
+                this.currentScene.destroy();
+            }
+            phaser.world.removeAll();
+            scene.create();
+            this.currentScene = scene;
+        }
+    }
+
+    public create():void {
+        console.log('typeof this: ' + (typeof this));
+        let start_screen:StartScreen = new StartScreen();
+        this.changeScene(start_screen);
+    }
+
+    public update():void {
+        if(this.currentScene) {
+            this.currentScene.update();
+        }
+    }
+
+    public render():void {
+        if(this.currentScene) {
+            this.currentScene.render();
+        }
     }
 };
 
+class Scene {
+    public create():void { };
+    public update():void { };
+    public render():void { };
+    public destroy():void { };
+};
 
-class SimpleGame {
-    constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+class StartScreen extends Scene {
+    public create():void {
+        let logo = phaser.add.sprite(phaser.world.centerX, phaser.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 1.0);
+        logo.scale.setTo(2.0, 2.0);
+        
+        let copyright_txt = phaser.add.bitmapText(104 * RESMULX, 119 * RESMULY, 'emulogic', '\xA91985 NINTENDO', 12 * RESMULX);
+        copyright_txt.tint = 0xfcbcb0;
+        phaser.add.bitmapText(28 * RESMULX, 142 * RESMULY, 'emulogic', 'PRESS SPACE/BTN1 TO START', 12 * RESMULX);
     }
     
-    game: Phaser.Game;
-    
-    preload() {
-        this.game.load.image('logo', 'phaser2.png');
+    public update():void {
+        
     }
     
-    create() {
-        let logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+    public render():void {
+        
+    }
+    
+    public destroy():void {
+        
     }
 };
 
-window.onload = () => {
-    let game = new SimpleGame();
+/*class InfoScreen extends Scene {
+
 };
 
+class LevelScene extends Scene {
 
-let greeter = new Greeting('Hello Phaser! again..');
+};
+*/
 
-document.body.innerHTML = greeter.greet();
+document.body.innerHTML = '';
+ 
+let game = new SMBGame();
